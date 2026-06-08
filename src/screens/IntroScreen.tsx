af94abdadd69addd3cef0131b6824cd9fdcd89b6
mudit sahu <muditsahu@mudits-MacBook-Air.local>
2026-06-08 23:15:20 +0530

    Setup project structure and intro screen

---
+++ src/screens/IntroScreen.tsx
@@ -0,0 +1,15 @@
+import { View, Text } from "react-native";
+
+export default function IntroScreen() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>CalorAI Intro Screen</Text>
+    </View>
+  );
+}
